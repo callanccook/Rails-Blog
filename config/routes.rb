2e2b33c913696a0fd5e44db2ee74419e5e710b38
Callan Cook <callan.c.cook@gmail.com>
2017-04-11 15:28:14 +0200

    added about, contact, blog pages, made index the root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  
+  get 'static_pages/about' , :as => 'about_page'
+
+  get 'static_pages/contact' , :as => 'contact_page'
+  get 'static_pages/blog' , :as => 'blog_page'
+  
+  get 'static_pages/index'
+  root 'static_pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
