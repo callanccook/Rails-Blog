87cd62c9fe693d4535a33ff16d7852f0d9b96a9f
Callan Cook <callan.c.cook@gmail.com>
2017-04-12 17:04:35 +0200

    altered html to accomodate new migration

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -69,6 +69,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:name, :description, :image_url)
+      params.require(:post).permit(:name, :description, :image_url, :body)
     end
 end
